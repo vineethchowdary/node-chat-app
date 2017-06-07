28ba9d231bd5b73be442d6f33abb9c3b2791b9eb
vineeth <vineeth3456@gmail.com>
2017-06-07 15:42:25 -0500

    saved

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ const path = require('path');
 const publicPath = path.join(__dirname,'../public');
 //console.log(__dirname + '/../public');
 //console.log(publicPath);
+const http = require('http');
 const port = process.env.PORT || 3000;
 const express = require('express');
+const socketIO = require('socket.io');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
+io.on('connection',(socket) => {
+
+  console.log('new user connected');
+  socket.on('disconnect',() => {
+    console.log('user disconnected');
+  });
+});
 
 app.listen(port,() => {
 
