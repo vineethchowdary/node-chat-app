fbd57285165aae4b354dd4aeeb21229c652c7ba8
vineeth <vineeth3456@gmail.com>
2017-06-07 14:20:30 -0500

    saved

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+//console.log(__dirname + '/../public');
+//console.log(publicPath);
+const port = process.env.PORT || 3000;
+const express = require('express');
+var app = express();
+
+
+app.use(express.static(publicPath));
+
+app.listen(port,() => {
+
+  console.log(`server is ready at ${port}`);
+});
