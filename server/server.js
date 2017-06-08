6fa3adc4bd86f4fabd8f9a272deef7bef958375b
vineeth <vineeth3456@gmail.com>
2017-06-07 19:13:35 -0500

    saved

--- server/server.js
+++ server/server.js
@@ -14,14 +14,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+socket.emit('newMessage',{
+from:'admin',
+text:'welcome to chat app',
+createdAt:new Date().getTime()
+
+});
+socket.broadcast.emit('newMessage',{
+
+from:'admin',
+text:'new user joined',
+createdAt:new Date().getTime()  
+});
+
+
 
 socket.on('createMessage',(message) => {
   console.log(message);
-  io.emit('newMessage',{
-    from:message.from,
-    text:message.text,
-    createdAt:new Date().getTime()
-  })
+   io.emit('newMessage',{
+     from:message.from,
+     text:message.text,
+     createdAt:new Date().getTime()
+})
+
+// socket.broadcast.emit('newMessage',{
+//   from:message.from,
+//   text:message.text,
+//   createdAt:new Date().getTime()
+// })
+
+
 });
 
   socket.on('disconnect', () => {
