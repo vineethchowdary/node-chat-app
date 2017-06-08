db50a2ae5b5174d745c8541080572ef041b79170
vineeth <vineeth3456@gmail.com>
2017-06-08 14:21:19 -0500

    saved

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,16 @@ socket.on('newMessage',function (message) {
   li.text(`${message.from}:${message.text}`);
   jQuery('#messages').append(li);
 });
+
+socket.on('newLocationMessage',function(message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}:`);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+
+});
 // socket.emit('createMessage',{
 //   from:'jammi',
 //   text:'hello how are you'
@@ -31,6 +41,29 @@ socket.emit('createMessage',{
 },function ()
 {
 
+});
+
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function()
+{
+  if(!navigator.geolocation)
+  {
+    return alert('geolocation not supported by browser');
+  }
+
+navigator.geolocation.getCurrentPosition(function(position){
+//console.log(position);
+
+socket.emit('createLocationMessage',{
+  latitude:position.coords.latitude,
+  longitude:position.coords.longitude
+});
+
+
+},function() {
+  alert('unable to fetch location..');
 });
 
 });
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-const {generateMessage} = require('./utils/message.js');
+const {generateMessage,generateLocationMessage} = require('./utils/message.js');
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
@@ -23,14 +23,13 @@ socket.on('createMessage',(message,callback) => {
   console.log(message);
    io.emit('newMessage',generateMessage(message.from,message.text));
    callback('this is from server');
-// socket.broadcast.emit('newMessage',{
-//   from:message.from,
-//   text:message.text,
-//   createdAt:new Date().getTime()
-// })
-
-
 });
+
+
+socket.on('createLocationMessage',(coords) => {
+  io.emit('newLocationMessage',generateLocationMessage('admin',coords.latitude,coords.longitude));
+});
+
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
